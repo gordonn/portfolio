cd00fa359944ec048408c98fd3d1714be3b51874
gordonn <tofugordon294@gmail.com>
2025-06-14 14:59:50 -0700

    update

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -1,3 +1,21 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return <div className="pb-96">
+    <SectionHeader
+      eyebrow="About Me"
+      title="A Glimpse Into My World"
+      description="Learn more about who I am what I do, and what inspires me." />
+    <div>
+      <Card>
+        <div>
+          <StarIcon />
+          <h3>My Reads</h3>
+          <p>Explore the books shaping my perspectives.</p>
+        </div>
+      </Card>
+    </div>
+  </div>;
 };
